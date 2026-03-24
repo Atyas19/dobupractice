c070ad0ea3c3f07b1af943a1e759d62265ace5e4
Atyas19 <a.yasin@stud.abingdon-witney.ac.uk>
2026-03-24 12:32:19 +0000

    New index type

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
+<?php
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create portal</title>
+    <title>DoBu Martial Arts</title>
 </head>
 <body>
 <p> this is an update</p>
