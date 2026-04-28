db1ce05f42698f10331890952cf3d7b81d023d62
Atyas19 <a.yasin@stud.abingdon-witney.ac.uk>
2026-04-28 11:01:55 +0100

    Making index page into the sign up page.

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
 <?php
-
-
+session_start();
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DoBu Martial Arts</title>
+    <title>DoBu Martial Arts Sign-up</title>
 </head>
 <body>
-    <h1>DoBu Martial Arts Website</h1>
+    <h1>DoBu Martial Arts website Sign up</h1>
 <p>Welcome to DoBu Martial Arts website, 
 Here we provide details and information about our facilities, courses, membership plans, 
 course timetable, members of staff and what they cover, private courses and age group courses,
-and relaxation and health beneficial facilities like the steam room.
+and relaxation and health beneficial facilities like the steam room. To learn more about DoBu
+and access more of the pages create an account using your details in the form below.
 </p>
+<br><br>
+<div class = "form">
+<h2>Sign-up form</h2>
+<form action="index.php" method="POST">
+    <label for ="fname">First_name:</label>
+        <input type="text" id="fname" name="fname" placeholder="John" required> <br>
+    <label for ="lname">Last_name</label>
 </body>
 </html> 
