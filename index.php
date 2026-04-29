c73405988078c6e5c26f852ba099cedaaa543037
Atyas19 <a.yasin@stud.abingdon-witney.ac.uk>
2026-04-29 10:21:55 +0100

    Login link

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ session_start();
 <header>
     <h1>DoBu Martial Arts website Sign up</h1>
     <nav>
-        <a herf="/pages/login.php">DoBu login</a>
+        <a herf="http://localhost/my_project/dobupractice/pages/login.php">DoBu login</a>
     </nav>
 </header>
 <body>
@@ -34,15 +34,21 @@ If you already have an account, go to the hamburger menu and select login.</p>
         <input type="text" id="lname" name="lname" placeholder="Doe" required> <br>
     <label for="email">Email:</label><br>
         <input type="email" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2, 4}$" placeholder="j.doe@gmail.co.uk" required><br>
-    <label for="pass_word">password</label>
+    <label for="pass_word">password:</label><br>
         <input type="password" id="pass_word" name="pass_word" required><br>
+    <label for="mobile">Mobile_number:</label><br>
+        <input type="tel" id="mobile" name="mobile" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"><br><br>
     <input type="submit" value="Sign up">
 </form>
 
 <!--This code will collect the customer data from the HTML form and insert it into the php database-->
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-
-    ?>
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $pass_word =password_hash($_POST['pass_word'], PASSWORD_DEFAULT);
+}
+?>
 </body>
 </html> 
